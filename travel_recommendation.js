604231c78fa17db2e12404ab36b6f8d01cdc6035
marcsosa <edwardmarcx@gmail.com>
2024-04-18 09:58:43 -0400

    Actualizar pagina

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,87 +1,74 @@
-function searchCondition() {
-    const input = document.getElementById("countryInput").value.trim().toLowerCase();
-    const resultDiv = document.getElementById("data-container");
-    resultDiv.innerHTML = "";
 
-    fetch("https://marcsosa.github.io/travel_recommendation/travel_recommendation_api.json")
+let jsonData;
+
+function resetBusqueda() {
+    document.getElementById('searchInput').value = '';
+    displayResults([]);
+}
+
+function ObtenerData() {
+    const keyword = document.getElementById('searchInput').value.trim().toLowerCase();
+    if (keyword === '') {
+        console.log('Invalid keyword please try again');
+        return;
+    }
+
+    fetch('./travel_recommendation_api.json')
         .then(response => {
             if (!response.ok) {
-                throw new Error("Network response was not ok");
+                throw new Error('Network response error: ' + response);
             }
             return response.json();
         })
         .then(data => {
-            const { beaches, temples, countries } = data;
-            if (countries) {
-                const country = countries.find(item => item.name.toLowerCase() === input);
-                if (country) {
-                    renderCities(country.cities);
-                } else if (beaches && beaches.some(beach => beach.name.toLowerCase() === input)) {
-                    renderItems(beaches);
-                } else if (temples && temples.some(temple => temple.name.toLowerCase() === input)) {
-                    renderItems(temples);
-                } else {
-                    displayError();
-                }
-            } else {
-                displayError();
-            }
+            jsonData = data;
+            filterResults(keyword);
         })
         .catch(error => {
-            console.error("Error:", error);
-            displayError();
+            console.error('There was a problem.. :', error);
         });
-
-    function renderCities(cities) {
-        resultDiv.innerHTML = "";
-        resultDiv.classList.add("bg-white", "overflow-y-scroll", "overflow-x-hidden", "py-4");
-        cities.forEach(city => {
-            resultDiv.innerHTML += `
-                <div class="mb-8 p-5">
-                    <h1 class="text-2xl font-bold mb-2">${city.name}</h1>
-                    <img src="${city.imageUrl}" alt="${city.name}" class="mb-2 w-96">
-                    <p class="text-lg">${city.description}</p>
-                </div>
-            `;
-        });
-    }
-
-    function renderItems(items) {
-        resultDiv.innerHTML = "";
-        resultDiv.classList.add("bg-white", "", "overflow-x-hidden", "py-4");
-        items.forEach(item => {
-            resultDiv.innerHTML += `
-                <div class="mb-8">
-                    <h1 class="text-2xl font-bold mb-2">${item.name}</h1>
-                    <img src="${item.imageUrl}" alt="${item.name}" class="mb-2 w-96">
-                    <p class="text-lg">${item.description}</p>
-                </div>
-            `;
-        });
-    }
-
-    function displayError() {
-        resultDiv.innerHTML = `<h1 class="text-4xl font-bold text-black text-center">¡No se encontró! Hubo un error.</h1>`;
-    }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("btnSearch").addEventListener("click", searchCondition);
-    document.getElementById("countryInput").addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            searchCondition();
-            clearResults();
-        }
+function filterResults(keyword) {
+    const filteredCities = [];
+
+    jsonData.countries.forEach(country => {
+        country.cities.forEach(city => {
+            if (city.name.toLowerCase().includes(keyword) || city.description.toLowerCase().includes(keyword)) {
+                filteredCities.push(city);
+            }
+        });
     });
-});
 
-function clearSearch() {
-    document.getElementById("countryInput").value = "";
-    clearResults();
+    if (keyword.toLowerCase().includes('temple')) {
+        jsonData.temples.forEach(temple => {
+            filteredCities.push(temple);
+        });
+    }
+
+    if (keyword.toLowerCase().includes('beach')) {
+        jsonData.beaches.forEach(beach => {
+            filteredCities.push(beach);
+        });
+    }
+
+    displayResults(filteredCities);
 }
 
-function clearResults() {
-    const resultDiv = document.getElementById("data-container");
-    resultDiv.innerHTML = "";
-    resultDiv.className = "";
+function displayResults(results) {
+    const cardsContainer = document.getElementById('displayData');
+    cardsContainer.innerHTML = '';
+
+    results.forEach(city => {
+        const card = `
+        <div class="bg-blue-500 text-center  text-white text-2xl rounded-2xl">${city.name}</div>
+        <div class="bg-white rounded-lg shadow-md my-1 p-4">
+            <img src="${city.imageUrl}" alt="${city.name}" class="w-full h-40 object-cover mb-4 rounded-md">
+            <h3 class="text-lg font-semibold mb-2">${city.name}</h3>
+            <p>${city.description}</p>
+            <button class=" mt-2 px-3 bg-blue-500 text-white py-2 rounded hover:bg-blue-800">Visitar</button>
+        </div>
+    `;
+        cardsContainer.innerHTML += card;
+    });
 }
